03bbb99a5aa93804f7e1042028f7648510c281b2
zhaoli <zhao-li@126.com>
2017-10-17 23:48:26 +0800

    测试redis

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,19 @@
 <?php
 namespace app\index\controller;
 
+use think\cache\driver\Redis;
+
 class Index
 {
     public function index()
     {
         return 'hello, zhaoliu!';
+    }
+
+    public function redis(){
+        $redis = new Redis();
+        $redis -> connect('127.0.0.1', 6379);
+        echo "sucessfully";
+        echo "redis".$redis->ping();
     }
 }
